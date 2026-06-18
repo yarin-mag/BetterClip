b117893c76729e020097b4b76683d98ac0277113
YARINSWINDOWSPC\yarin <yarinmagdaci@gmail.com>
2026-06-18 21:06:47 +0300

    feat: add app and menu bar icons

--- BetterClip/App/AppDelegate.swift
+++ BetterClip/App/AppDelegate.swift
@@ -28,8 +28,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     private func setupMenuBar() {
-        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-        statusItem.button?.title = "📋"
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        statusItem.button?.image = StatusBarIcon.makeImage()
         statusItem.button?.toolTip = "BetterClip"
 
         let menu = NSMenu()
@@ -106,5 +106,40 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let delegate = AppDelegate()
         app.delegate = delegate
         app.run()
+    }
+}
+
+private enum StatusBarIcon {
+    static func makeImage() -> NSImage {
+        let size = NSSize(width: 18, height: 18)
+        let image = NSImage(size: size)
+        image.lockFocus()
+
+        NSColor.black.setStroke()
+        NSColor.black.setFill()
+
+        let body = NSBezierPath(roundedRect: NSRect(x: 4, y: 3, width: 10, height: 12),
+                                xRadius: 2,
+                                yRadius: 2)
+        body.lineWidth = 1.6
+        body.stroke()
+
+        let clip = NSBezierPath(roundedRect: NSRect(x: 6.2, y: 13, width: 5.6, height: 2.4),
+                                xRadius: 1.2,
+                                yRadius: 1.2)
+        clip.fill()
+
+        let cut = NSBezierPath()
+        cut.move(to: NSPoint(x: 7, y: 10.8))
+        cut.line(to: NSPoint(x: 11, y: 7))
+        cut.move(to: NSPoint(x: 11, y: 10.8))
+        cut.line(to: NSPoint(x: 7, y: 7))
+        cut.lineWidth = 1.45
+        cut.lineCapStyle = .round
+        cut.stroke()
+
+        image.unlockFocus()
+        image.isTemplate = true
+        return image
     }
 }
